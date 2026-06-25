922139c83d82d521fd97257e45ddfa22b12c01a8
Nitheen.M <nitheen111026@gmail.com>
2026-06-25 10:48:45 +0530

    Refactor app.py to improve readability and structure

--- app.py
+++ app.py
@@ -143,8 +143,6 @@ def generate_full_report(df, cat_col, val_col, date_col):
         elements.append(Spacer(1, 150))
         elements.append(Image(logo_path, width=300, height=200))
 
-    elements.append(Spacer(1, 15))
-    elements.append(Paragraph('<font name="Times-Roman" size="15">SMART ANALYTICS MACHINE</font>', styles["Title"]))
     elements.append(Spacer(1, 28))
     elements.append(Paragraph('<font name="Times-Roman" size="28">DEVELOPED BY NITHEEN.M</font>', styles["Title"]))
     elements.append(Spacer(1, 15))
